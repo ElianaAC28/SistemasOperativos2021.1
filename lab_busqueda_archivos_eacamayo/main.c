83136a78406ea83eb82f381572143b4da385d604
ElianaAC28 <eacamayo@unicauca.edu.co>
2021-03-08 10:40:41 -0500

    falta identar, ya funciona

--- lab_busqueda_archivos_eacamayo/main.c
+++ lab_busqueda_archivos_eacamayo/main.c
@@ -11,12 +11,8 @@
 //Ultima modificación: 06/03/2021
 
 void help(void);
-DIR * d;
-struct dirent * ent;
-char * fullPath;
-int indent=0;
-int varConcide=0;
-int search ( char * dir, char * pattern, int indent);
+
+int search ( char * dir, char * pattern);
 
 int main(int argc, char * argv[]){
 	char * dir;
@@ -36,14 +32,10 @@ int main(int argc, char * argv[]){
 		dir = argv[1];
 		pattern = argv[2];
 	}
+int total = search(dir, pattern);
+	 printf("Total concidencias : %d\n", total);
 
-	memset(ent,0,0);
-	varConcide= search(dir, pattern, 0);
-		if(varConcide > 0){
-			printf("Total concidencias : %d\n, varConcide");
-		}
 		
-		printf("TODO bucar %s en %s\n", pattern, dir, indent);
 exit(EXIT_SUCCESS);
 }
 
@@ -51,8 +43,13 @@ void help (void){
 	fprintf(stdout, "Uso :	./main ARCHIVO Busca un archivo en directorio actual y subdirectorios .\n");
 	fprintf(stdout, "	./main DIR ARCHIVO Busca un archivo en el directorio DIR y subdirectorios. \n");
 }
-int search(char * dir, char * pattern, int indent){
+int search(char * dir, char * pattern){
 	
+  int varConcide=0;
+  DIR * d;
+  struct dirent * ent;
+	char * fullPath;
+	struct stat ss;
 	//Se usa opendir sobre dir para obtener apuntador a una estructura DIR
 	d = opendir(dir);
 
@@ -63,12 +60,8 @@ int search(char * dir, char * pattern, int indent){
 
 	//Recorre el directorio, se llama a readdir para leer la siguiente entreda del directorio
 	while((ent = readdir(d)) != NULL){
-  printf("Entrada: %s\n", ent->d_name);
+  //printf("Entrada: %s\n", ent->d_name);
 	
-	//Verificar si el nombre del archivo o directorio coincide con el patrón
-	if(strstr(ent->d_name, pattern) != NULL) {
-			printf("El nombre del archivo o director coincide con el padrón\n");
-			
 			//Obtener la ruta completa
 			
 			//reservar memoria
@@ -76,33 +69,33 @@ int search(char * dir, char * pattern, int indent){
 			
 			strcpy(fullPath, dir);
 			strcat(fullPath,"/");
-			fullPath = strcat(fullPath, ent-> d_name);
+			fullPath = strcat(fullPath, ent-> d_name);	
+
+	//Verificar si el nombre del archivo o directorio coincide con el patrón
+	if(strstr(ent->d_name, pattern) != NULL) {
+		//	printf("El nombre del archivo o director coincide con el patrón\n");
 			
 			printf("Ruta completa: %s \n", fullPath);
 		  
 			varConcide = varConcide + 1;
-			
+	}
+	//Verificar si es un directori
+
+		if(stat(fullPath,&ss)==-1){
+			perror("stat");
+			//	printf("No se puede acceder AQUI  la ruta \n");
+		
 		}
-	//Verificar si es un directorio
-		if (ent->d_type == DT_DIR) {
-			char path[1024];
+			if (S_ISDIR(ss.st_mode)) {
 
 			//Ignorar entradas con d_name "." y ".."
 			if(strcmp(ent->d_name, ".")==0 || strcmp(ent->d_name, "..")==0){
-				printf("Peligro! %s\n", ent->d_name);
+			//	printf("Peligro! %s\n", ent->d_name);
 				continue;
 			}
-			
-				snprintf(path, sizeof(path), "%s/%s", dir, ent->d_name);
-        printf("%*s[%s]\n", indent, "", ent->d_name);
-
-			return search(path, pattern, indent + 2);
-		} else {
-
-            printf("%*s- %s\n", indent, "", ent->d_name);
+			varConcide = varConcide + search(fullPath, pattern);	
 		}
-
-	}
-
+}
+	closedir(d); 
 	return varConcide;
 }
